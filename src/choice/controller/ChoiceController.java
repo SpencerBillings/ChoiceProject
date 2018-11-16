2a484120ee3768eba8594aa3d734edcb09e40ed7
Spencer B <spencerbillings2000@gmail.com>
2018-11-16 09:22:21 -0700

    Heavily fleshed out the model and controller packages and added Cake images

--- src/choice/controller/ChoiceController.java
+++ src/choice/controller/ChoiceController.java
@@ -1,17 +1,53 @@
 package choice.controller;
 
 import choice.model.Cake;
+
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class ChoiceController
 {
+	private Cake defaultCake;
+	private Cake [] cakeArray;
+	private ImageIcon [] icons;
+	
 	public ChoiceController()
 	{
-		tastyCake = new Cake();
+		defaultCake = new Cake(0, 10.0, "[Insert Text]");
+		
+		cakeArray = new Cake[3];
+		
+		icons = new ImageIcon[5];
+		icons[0] = new ImageIcon(getClass().getResource("/choice/view/images/Cake1.jpg"));
+		icons[1] = new ImageIcon(getClass().getResource("/choice/view/images/Cake2.jpg"));
+		icons[2] = new ImageIcon(getClass().getResource("/choice/view/images/Cake3.jpg"));
+		icons[3] = new ImageIcon(getClass().getResource("/choice/view/images/Cake4.jpg"));
+		icons[4] = new ImageIcon(getClass().getResource("/choice/view/images/Cake5.jpg"));
+		icons[5] = new ImageIcon(getClass().getResource("/choice/view/images/Cake6.jpg"));
+		
 	}
 	
 	public void start()
 	{
 		
+	}
+	
+	private void arrayInit()
+	{
+		JOptionPane.showMessageDialog(null, "Look at this cake!", "Arrays", JOptionPane.INFORMATION_MESSAGE, icons[1]);
+		JOptionPane.showMessageDialog(null, "Now, look at this cake!", "Arrays", JOptionPane.INFORMATION_MESSAGE, icons[2]);
+		JOptionPane.showMessageDialog(null, "Now, look at THIS cake!", "Arrays", JOptionPane.INFORMATION_MESSAGE, icons[3]);
+		
+		for (int index = 0; index < cakeArray.length; index += 1)
+		{
+			if(index % 2 == 0)
+			{
+				cakeArray[index] = new Cake();
+			}
+			else
+			{
+				cakeArray[index] = new Cake(index * 30);
+			}
+		}
 	}
 }
--- src/choice/model/Cake.java
+++ src/choice/model/Cake.java
@@ -2,6 +2,51 @@ package choice.model;
 
 public class Cake 
 {
+	private int cakeType;
 	private double cakeSize;
-	private 
+	private String cakeText;
+	
+	public Cake(int cakeType, double cakeSize, String cakeText)
+	{
+		this.cakeType = cakeType;
+		this.cakeSize = cakeSize;
+		this.cakeText = cakeText;
+	}
+	
+	// Getters
+	public int cakeType()
+	{
+		return cakeType;
+	}
+	
+	public double cakeSize()
+	{
+		return cakeSize;
+	}
+	
+	public String cakeText()
+	{
+		return cakeText;
+	}
+	
+	// Setters
+	public void cakeType(int typeOfCake)
+	{
+		this.cakeType = typeOfCake;
+	}
+	
+	public void cakeSize(double sizeOfCake)
+	{
+		this.cakeSize = sizeOfCake;
+	}
+	
+	public void cakeText(String textOnCake)
+	{
+		this.cakeText = textOnCake;
+	}
+	
+	// public String toString()
+	{
+		//String desc
+	}
 }
